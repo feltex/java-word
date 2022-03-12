d9e91bd56d4b6709de291d376d30cda7f2b615f6
Andre Felix <andrefelix.br@gmail.com>
2022-03-12 16:22:02 +0000

    Initial commit

--- src/main/java/br/com/feltex/avancado/CriaArquivo.java
+++ src/main/java/br/com/feltex/avancado/CriaArquivo.java
@@ -36,65 +36,65 @@ public class CriaArquivo {
     }
 
     private void adicionarTitulo(final XWPFDocument documento, final String titulo) {
-        XWPFParagraph title = documento.createParagraph();
-        title.setAlignment(ParagraphAlignment.CENTER);
-        XWPFRun titleRun = title.createRun();
-        titleRun.setText(titulo);
-        titleRun.setColor("6495ED");
-        titleRun.setBold(true);
-        titleRun.setFontFamily(FONT_COURIER);
-        titleRun.setFontSize(20);
+        XWPFParagraph paragrafoTitulo = documento.createParagraph();
+        paragrafoTitulo.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun tituloRun = paragrafoTitulo.createRun();
+        tituloRun.setText(titulo);
+        tituloRun.setColor("6495ED");
+        tituloRun.setBold(true);
+        tituloRun.setFontFamily(FONT_COURIER);
+        tituloRun.setFontSize(20);
     }
 
     private void adicionarSubTitulo(final XWPFDocument documento, final String subtitulo) {
-        XWPFParagraph subTitle = documento.createParagraph();
-        subTitle.setAlignment(ParagraphAlignment.CENTER);
-        XWPFRun subTitleRun = subTitle.createRun();
-        subTitleRun.setText(subtitulo);
-        subTitleRun.setColor("00CC44"); // Verde
-        subTitleRun.setFontFamily(FONT_COURIER);
-        subTitleRun.setFontSize(16);
-        subTitleRun.setTextPosition(20);
-        subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
+        XWPFParagraph paragrafoSubTitulo = documento.createParagraph();
+        paragrafoSubTitulo.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun subtituloRun = paragrafoSubTitulo.createRun();
+        subtituloRun.setText(subtitulo);
+        subtituloRun.setColor("00CC44"); // Verde
+        subtituloRun.setFontFamily(FONT_COURIER);
+        subtituloRun.setFontSize(16);
+        subtituloRun.setTextPosition(20);
+        subtituloRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
     }
 
     private void adicionarImagem(final XWPFDocument documento, final String imagem) throws IOException, URISyntaxException, InvalidFormatException {
         XWPFParagraph paragrafoComImagem = documento.createParagraph();
         paragrafoComImagem.setAlignment(ParagraphAlignment.CENTER);
-        XWPFRun imageRun = paragrafoComImagem.createRun();
-        imageRun.setTextPosition(20);
+        XWPFRun imagemRun = paragrafoComImagem.createRun();
+        imagemRun.setTextPosition(20);
         var imagemPath = Paths.get(ClassLoader.getSystemResource(imagem).toURI());
-        imageRun.addPicture(Files.newInputStream(imagemPath), Document.PICTURE_TYPE_PNG,
+        imagemRun.addPicture(Files.newInputStream(imagemPath), Document.PICTURE_TYPE_PNG,
                 imagemPath.getFileName().toString(), Units.toEMU(50), Units.toEMU(50));
 
     }
 
     private void adicionarParagrafo(final XWPFDocument documento, final String conteudo) {
-        XWPFParagraph para1 = documento.createParagraph();
-        para1.setAlignment(ParagraphAlignment.BOTH);
-        XWPFRun para1Run = para1.createRun();
+        XWPFParagraph paragrafo = documento.createParagraph();
+        paragrafo.setAlignment(ParagraphAlignment.BOTH);
+        XWPFRun para1Run = paragrafo.createRun();
         para1Run.setText(conteudo);
     }
 
     private void adicionarSaudacao(final XWPFDocument documento, final String saudacao) {
 
-        XWPFParagraph sectionTitle = documento.createParagraph();
-        XWPFRun sectionTRun = sectionTitle.createRun();
-        sectionTRun.setText(saudacao);
-        sectionTRun.setColor("00CC44");
-        sectionTRun.setBold(true);
-        sectionTRun.setFontFamily(FONT_COURIER);
+        XWPFParagraph paragrafoSecao = documento.createParagraph();
+        XWPFRun secaoRun = paragrafoSecao.createRun();
+        secaoRun.setText(saudacao);
+        secaoRun.setColor("00CC44");
+        secaoRun.setBold(true);
+        secaoRun.setFontFamily(FONT_COURIER);
     }
 
     private void adicionarTabela(final XWPFDocument documento, final List<Produto> produtos) {
 
         var tabela = documento.createTable();
-        XWPFTableRow tableRowOne = tabela.getRow(0);
-        tableRowOne.getCell(0).setText("Codigo");
-        tableRowOne.addNewTableCell().setText("Nome");
-        tableRowOne.addNewTableCell().setText("Preco");
-        tableRowOne.addNewTableCell().setText("Quantidade");
-        tableRowOne.addNewTableCell().setText("Total");
+        XWPFTableRow linha = tabela.getRow(0);
+        linha.getCell(0).setText("Código");
+        linha.addNewTableCell().setText("Nome");
+        linha.addNewTableCell().setText("Preço");
+        linha.addNewTableCell().setText("Quantidade");
+        linha.addNewTableCell().setText("Total");
 
         produtos.forEach(p -> criarLinha(tabela, p));
 
--- src/main/java/br/com/feltex/basico/CriarArquivoBasico.java
+++ src/main/java/br/com/feltex/basico/CriarArquivoBasico.java
@@ -4,88 +4,72 @@ import org.apache.poi.util.Units;
 import org.apache.poi.xwpf.usermodel.*;
 
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CriarArquivoBasico {
 
-    private static String logo = "avatar.png";
-    private static String paragraph1 = "arquivo1.txt";
-    private static String paragraph2 = "arquivo2.txt";
-    private static String paragraph3 = "arquivo3.txt";
-    private static String output = "relatórioPedidos.docx";
+    private static String output = "HistoriaDoDia.docx";
     private static final String FONT_COURIER = "Courier";
 
     public static void main(String[] args) throws Exception {
         try (var document = new XWPFDocument()) {
 
-            XWPFParagraph title = document.createParagraph();
-            title.setAlignment(ParagraphAlignment.CENTER);
-            XWPFRun titleRun = title.createRun();
-            titleRun.setText("Confirmacão de compras");
-            titleRun.setColor("6495ED");
-            titleRun.setBold(true);
-            titleRun.setFontFamily(FONT_COURIER);
-            titleRun.setFontSize(20);
+            XWPFParagraph titulo = document.createParagraph();
+            titulo.setAlignment(ParagraphAlignment.CENTER);
+            XWPFRun tituloRun = titulo.createRun();
+            tituloRun.setText("Batatinha Quando Nasce");
+            tituloRun.setColor("6495ED");
+            tituloRun.setBold(true);
+            tituloRun.setFontFamily(FONT_COURIER);
+            tituloRun.setFontSize(20);
 
-            XWPFParagraph subTitle = document.createParagraph();
-            subTitle.setAlignment(ParagraphAlignment.CENTER);
-            XWPFRun subTitleRun = subTitle.createRun();
-            subTitleRun.setText("Este email é confidencial e direcionado ao cliente.");
-            subTitleRun.setColor("00CC44");
-            subTitleRun.setFontFamily(FONT_COURIER);
-            subTitleRun.setFontSize(16);
-            subTitleRun.setTextPosition(20);
-            subTitleRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
+            XWPFParagraph subTitulo = document.createParagraph();
+            subTitulo.setAlignment(ParagraphAlignment.CENTER);
+            XWPFRun subTituloRun = subTitulo.createRun();
+            subTituloRun.setText("Esta é uma história infantil");
+            subTituloRun.setColor("00CC44");
+            subTituloRun.setFontFamily(FONT_COURIER);
+            subTituloRun.setFontSize(16);
+            subTituloRun.setTextPosition(20);
+            subTituloRun.setUnderline(UnderlinePatterns.DOT_DOT_DASH);
 
-            XWPFParagraph image = document.createParagraph();
-            image.setAlignment(ParagraphAlignment.CENTER);
-            XWPFRun imageRun = image.createRun();
-            imageRun.setTextPosition(20);
-            var imagePath = Paths.get(ClassLoader.getSystemResource(logo).toURI());
-            imageRun.addPicture(Files.newInputStream(imagePath), Document.PICTURE_TYPE_PNG, imagePath.getFileName().toString(), Units.toEMU(50), Units.toEMU(50));
+            XWPFParagraph imagem = document.createParagraph();
+            imagem.setAlignment(ParagraphAlignment.CENTER);
+            XWPFRun imagemRun = imagem.createRun();
+            imagemRun.setTextPosition(20);
+            var imagePath = Paths.get(ClassLoader.getSystemResource("batatinha.jpg").toURI());
+            imagemRun.addPicture(Files.newInputStream(imagePath), Document.PICTURE_TYPE_PNG, imagePath.getFileName().toString(), Units.toEMU(50), Units.toEMU(50));
 
-            XWPFParagraph sectionTitle = document.createParagraph();
-            XWPFRun sectionTRun = sectionTitle.createRun();
-            sectionTRun.setText("Detalhes do Pedido.");
-            sectionTRun.setColor("00CC44");
-            sectionTRun.setBold(true);
-            sectionTRun.setFontFamily(FONT_COURIER);
+            XWPFParagraph secao = document.createParagraph();
+            XWPFRun secaoRun = secao.createRun();
+            secaoRun.setText("Segue a história.");
+            secaoRun.setColor("00CC44");
+            secaoRun.setBold(true);
+            secaoRun.setFontFamily(FONT_COURIER);
 
-            XWPFParagraph para1 = document.createParagraph();
-            para1.setAlignment(ParagraphAlignment.BOTH);
-            var string1 = convertTextFileToString(paragraph1);
-            XWPFRun para1Run = para1.createRun();
-            para1Run.setText(string1);
+            XWPFParagraph paragrafo1 = document.createParagraph();
+            paragrafo1.setAlignment(ParagraphAlignment.BOTH);
+            XWPFRun para1Run = paragrafo1.createRun();
+            para1Run.setText("Batatinha quando nasce \n Espalha a rama pelo chão");
 
-            XWPFParagraph para2 = document.createParagraph();
-            para2.setAlignment(ParagraphAlignment.RIGHT);
-            var string2 = convertTextFileToString(paragraph2);
-            XWPFRun para2Run = para2.createRun();
-            para2Run.setText(string2);
+            XWPFParagraph paragrafo2 = document.createParagraph();
+            paragrafo2.setAlignment(ParagraphAlignment.RIGHT);
+            XWPFRun para2Run = paragrafo2.createRun();
+            para2Run.setText("O brotinho quando ama\n Poe a mão, no coração");
             para2Run.setItalic(true);
 
-            XWPFParagraph para3 = document.createParagraph();
-            para3.setAlignment(ParagraphAlignment.LEFT);
-            var string3 = convertTextFileToString(paragraph3);
-            XWPFRun para3Run = para3.createRun();
-            para3Run.setText(string3);
+            XWPFParagraph paragrafo3 = document.createParagraph();
+            paragrafo3.setAlignment(ParagraphAlignment.LEFT);
+            XWPFRun para3Run = paragrafo3.createRun();
+            para3Run.setText("Menininha quando dorme\nPõe a mão no coração");
 
             try (var out = new FileOutputStream(output)) {
                 document.write(out);
             }
         }
-
     }
 
-    public static String convertTextFileToString(String fileName) throws URISyntaxException, IOException {
-        try (Stream<String> stream = Files.lines(Paths.get(ClassLoader.getSystemResource(fileName).toURI()))) {
-            return stream.collect(Collectors.joining(" "));
-        }
-    }
+
 
 }
